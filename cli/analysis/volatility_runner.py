bbacac3b6a6390273707f6ae2f5be32d2a3cd6ed
sunandabandi <sunandabandi28@gmail.com>
2026-05-15 12:07:51 +0530

    Fixed cross-platform Volatility execution for Windows and Linux

--- cli/analysis/volatility_runner.py
+++ cli/analysis/volatility_runner.py
@@ -1,11 +1,34 @@
 import subprocess
+import platform
+import os
 
 
 def run_plugin(memory_dump, plugin):
 
+    base_dir = os.path.dirname(
+        os.path.dirname(__file__)
+    )
+
+    vol_path = os.path.abspath(
+        os.path.join(
+            base_dir,
+            "..",
+            "volatility3",
+            "vol.py"
+        )
+    )
+
+    if platform.system() == "Windows":
+
+        python_cmd = "python"
+
+    else:
+
+        python_cmd = "python3"
+
     command = [
-        "python3",
-        "../volatility3/vol.py",
+        python_cmd,
+        vol_path,
         "-f",
         memory_dump,
         plugin
